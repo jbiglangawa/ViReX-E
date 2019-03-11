3d96dd7ca32f7e6c472880d92efd5e1502ba0e39
jmba909 <36124096+jmba909@users.noreply.github.com>
2019-03-11 21:32:02 +0800

    Arduino
    Codes for the Arduino.

---
+++ Arduino/Foot_Tracker_Alpha_2.0/Foot_Tracker/Foot_Tracker.ino
@@ -0,0 +1,59 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+#define CE_PIN 10
+#define CSN_PIN 9
+
+#define WHICH_NODE 1     // must be a number from 1 - 6 identifying the PTX node
+
+const uint64_t rAddress[] = {0x7878787878LL, 0xB3B4B5B6F1LL};
+const uint64_t PTXpipe = rAddress[ WHICH_NODE - 1 ];
+
+RF24 radio(CE_PIN, CSN_PIN); // Create a  Radio
+
+MPU6050 accelgyro;
+
+int data[8];
+bool SendingStatus;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+void setup() {
+    Wire.begin();
+    radio.begin();
+    radio.setRetries(3,5);
+    radio.setChannel(108); 
+    radio.setDataRate(RF24_250KBPS);//set data rate to 250kbps
+    radio.openReadingPipe(WHICH_NODE-1, rAddress[WHICH_NODE-1]);
+    radio.setPALevel(RF24_PA_LOW);
+    radio.enableAckPayload();
+    radio.startListening();
+    
+    accelgyro.initialize();
+    accelgyro.setFullScaleAccelRange(3);
+    accelgyro.setFullScaleGyroRange(3);
+    
+}
+
+void loop() {
+    if(radio.available()){
+      bool tmp;
+      radio.read(&tmp, sizeof(tmp));
+      accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      data[0] = ax;
+      data[1] = ay;
+      data[2] = az;
+      data[3] = gx;
+      data[4] = gy;
+      data[5] = gz;
+      data[6] = 4214;
+      radio.writeAckPayload(WHICH_NODE-1, &data, sizeof(data));
+    }
+    
+}
